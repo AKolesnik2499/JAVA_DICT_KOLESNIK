9acb599979365f4ac33e689baf64995f70aff8aa
Akolesnik2499 <o.o.koliesnik@student.khai.edu>
2021-12-23 08:16:28 +0300

    Add coffe.java

--- src/matrix/matrix.java
+++ src/matrix/matrix.java
@@ -1,6 +1,7 @@
 package matrix;
 import java.util.Scanner;
-public class main {
+
+public class matrix {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int choice = 0;
